8373028e69e8c81388b73bd1439f5783a59e96b5
Lukasz <chlebeke.lukasz@gmail.com>
2022-04-22 22:58:41 +0200

    Visual changes

--- map_route/src/MapPage.js
+++ map_route/src/MapPage.js
@@ -1,14 +1,41 @@
-import "./MainPage.css"
+import "./MapPage.css"
 import {Link} from "react-router-dom"
-import arrow from "./arrow-left-long-solid.svg"
-function MapPage(props){
+import {useLocation} from 'react-router-dom';
+import {DataHistory} from "./DataHistory";
+import Map from "./Map";
+import {useState} from "react";
+import KmComponent from "./KmComponent";
+import "./Range.css"
+
+
+function MapPage(){
+    const [rangeval, setRangeval] = useState(null);
+    const location = useLocation()
     return(
-        <div className={"container"}>
-            <Link to={"/"} style={{ textDecoration: 'none' }}>
-                <div className={"arrow"}>
-                    To main page
-                </div>
-            </Link>
+        <div className={"container-xxl"}>
+            <KmComponent props={{data:DataHistory.find(x=> x.idA+x.idB === location.state.idOfTile), rate: rangeval}}/>
+            <div className={"range-outputs-container"}>
+                <p className={"custom-range-label"}>Price for km </p>
+                <input type="range" className="custom-range" min="1" max="50"
+                       onChange={(event) => setRangeval(event.target.value)} />
+            </div>
+            <div className={"range-outputs-container"}>
+                <p className={"range-outputs"}>{rangeval}</p>
+                <p>-</p>
+                <p className={"range-outputs"}>50</p>
+            </div>
+            <div className={"map"}>
+                <Map props={DataHistory.find(x=> x.idA+x.idB === location.state.idOfTile)}/>
+            </div>
+            <div className={"buttons-map"}>
+                <Link to={"/"} style={{ textDecoration: 'none' }}>
+                    <div className={"button button-map-page"}>
+                        To main page
+                    </div>
+                </Link>
+
+            </div>
+
         </div>
     )
 }

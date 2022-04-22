4f1f23f80183abf44cb47e1c2e4f592459c46d20
Lukasz <chlebeke.lukasz@gmail.com>
2022-04-22 23:20:46 +0200

    Add export button and export feature

--- map_route/src/MapPage.js
+++ map_route/src/MapPage.js
@@ -3,16 +3,21 @@ import {Link} from "react-router-dom"
 import {useLocation} from 'react-router-dom';
 import {DataHistory} from "./DataHistory";
 import Map from "./Map";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import KmComponent from "./KmComponent";
 import "./Range.css"
+import ReactToPrint, {useReactToPrint} from "react-to-print";
 
 
 function MapPage(){
     const [rangeval, setRangeval] = useState(null);
     const location = useLocation()
+    const componentRef = useRef()
+    const handlePrint = useReactToPrint({
+        content: () =>componentRef.current
+    })
     return(
-        <div className={"container-xxl"}>
+        <div className={"container-xxl"} ref={componentRef}>
             <KmComponent props={{data:DataHistory.find(x=> x.idA+x.idB === location.state.idOfTile), rate: rangeval}}/>
             <div className={"range-outputs-container"}>
                 <p className={"custom-range-label"}>Price for km </p>
@@ -33,9 +38,8 @@ function MapPage(){
                         To main page
                     </div>
                 </Link>
-
+                <button  className={"button button-map-page"}onClick={handlePrint}>Export</button>
             </div>
-
         </div>
     )
 }

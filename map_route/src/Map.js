5edba179a1395f3d0dbd8b4c4e70170e51d580c7
Lukasz <chlebeke.lukasz@gmail.com>
2022-04-23 17:35:43 +0200

    Refactor Code

--- map_route/src/Map.js
+++ map_route/src/Map.js
@@ -5,9 +5,7 @@ import onResize from 'simple-element-resize-detector';
 export default class Map extends React.Component {
     constructor(props) {
         super(props);
-        // the reference to the container
         this.ref = React.createRef();
-        // reference to the map
         this.map = null;
     }
 
@@ -16,22 +14,17 @@ export default class Map extends React.Component {
             const routingParameters = {
                 'routingMode': 'fast',
                 'transportMode': 'car',
-                // The start point of the route:
                 'origin': this.props.props.pointACoord,
-                // The end point of the route:
                 'destination': this.props.props.pointBCoord,
-                // Include the route shape in the response
                 'return': 'polyline'
             };
-            // instantiate a platform, default layers and a map as usual
             const platform = new H.service.Platform({
                 apikey: 'ZqHI-mJG9L4fEibpuqHBlpvi2ju4FNxBGf-RNe-l1FM'
             });
-            var service = platform.getOMVService({path:  'v2/vectortiles/core/mc'});
-            // create a provider and a layer that use the custom service and a custom style
-            var provider = new H.service.omv.Provider(service,
+            let service = platform.getOMVService({path: 'v2/vectortiles/core/mc'});
+            let provider = new H.service.omv.Provider(service,
                 new H.map.Style('https://js.api.here.com/v3/3.1/styles/omv/oslo/japan/normal.day.yaml'));
-            var layer = new H.map.layer.TileLayer(provider, {max: 22});
+            let layer = new H.map.layer.TileLayer(provider, {max: 22});
 
             const map = new H.Map(
                 this.ref.current,
@@ -41,38 +34,28 @@ export default class Map extends React.Component {
                     center: {lat: 0, lng: 0},
                     zoom: 0,
                 },
-
-
             );
-            const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+            new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
             onResize(this.ref.current, () => {
                 map.getViewPort().resize();
             });
             this.map = map;
             const onResult = function(result) {
-                // ensure that at least one route was found
                 if (result.routes.length) {
                     result.routes[0].sections.forEach((section) => {
-                        // Create a linestring to use as a point source for the route line
                         let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
 
-                        // Create a polyline to display the route:
                         let routeLine = new H.map.Polyline(linestring, {
                             style: { strokeColor: '#1EDE00', lineWidth: 3 }
                         });
 
-                        // Create a marker for the start point:
                         let startMarker = new H.map.Marker(section.departure.place.location);
 
-                        // Create a marker for the end point:
                         let endMarker = new H.map.Marker(section.arrival.place.location);
 
-                        // Add the route polyline and the two markers to the map:
                         map.addObjects([routeLine, startMarker, endMarker]);
 
-                        // Set the map's viewport to make the whole route visible:
                         map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
-
                     });
                 }
             };
@@ -81,7 +64,7 @@ export default class Map extends React.Component {
                 function(error) {
                     alert(error.message);
                 });
-            var ui = new H.ui.UI.createDefault(map, layer)
+            let ui = new H.ui.UI.createDefault(map, layer);
             ui.getControl("mapsettings").setVisibility(false)
             ui.getControl("scalebar").setAlignment('top-left')
             ui.getControl('zoom').setAlignment('top-left');

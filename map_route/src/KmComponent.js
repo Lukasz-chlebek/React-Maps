1eda918ab2d5963345fc333f45220f50a9f64ab4
Lukasz <chlebeke.lukasz@gmail.com>
2022-04-23 17:36:21 +0200

    Refactor Code and added const values

--- map_route/src/KmComponent.js
+++ map_route/src/KmComponent.js
@@ -3,13 +3,12 @@ import {useState} from "react";
 import "./KmComponent.css"
 
 function KmComponent(props){
-    const Km = 1000, min=60, sumRate=1.1
+    const km = 1000, min=60, sumRate=1.1, maxCostPerDay=1000, maxLenPerDay=800
     const [data, setData]= useState({})
     const [state, setState] = useState(true)
     if(state){
         axios.get(`https://router.hereapi.com/v8/routes?transportMode=car&origin=${props.props.data.pointACoord}&destination=${props.props.data.pointBCoord}&return=summary&apiKey=ZqHI-mJG9L4fEibpuqHBlpvi2ju4FNxBGf-RNe-l1FM`)
             .then(res =>{
-                console.log(res)
                 setData({
                     length: res.data.routes[0].sections[0].summary.length,
                     duration: res.data.routes[0].sections[0].summary.duration,
@@ -19,13 +18,13 @@ function KmComponent(props){
                 console.log(e)
         })
     }
-    let cost = (data.length/Km)*props.props.rate*sumRate
-    let lengthInKm = (data.length/Km)
+    let cost = (data.length/km)*props.props.rate*sumRate
+    let lengthInKm = (data.length/km)
     let days;
-    if(cost/1000 > lengthInKm/800){
-        days = Math.ceil(cost/1000)
+    if(cost/maxCostPerDay > lengthInKm/maxLenPerDay){
+        days = Math.ceil(cost/maxCostPerDay)
     }else{
-        days = Math.ceil(lengthInKm/800)
+        days = Math.ceil(lengthInKm/maxLenPerDay)
     }
 
     return(
@@ -54,7 +53,6 @@ function KmComponent(props){
                 </div>
             </div>
         </div>
-
     )
 }
 
